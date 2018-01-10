72ae068a664fec1d0212c27c6d58eb6cc7e84b37
clchenliang <1035062871@qq.com>
2018-01-10 14:05:06 +0800

    子递归方式，正在尝试 ing~

---
+++ 010-Regular Expression Matching.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var isMatch = function(s,p) {
+    
+}
+var isSubMatch = function(s, p) {
+    if(s.length < minLen(p)) return false;
+    if(p.length === 0) return true;
+    else {
+        if(p[0] === '.') return isSubMatch(s.substr(1), p.substr(1));
+        if(p[0] === '*' ) return isSubMatch(s.substr());
+        if(p[1] && p[1] === '*') 
+            if(s[0] === p[0]) return isSubMatch(s.substr(1), p.substr(1)) || isSubMatch(s.substr(1),p);
+            else return false;
+        if(p[0] === s[0]) return isSubMatch(s.substr(1),p.substr(1));
+        return isSubMatch(s.substr(1),p);
+    }
+    
+};
+function minLen(p) {
+    var len = p.length;
+    for(var i in p) {
+        if(p[i] === '*') len--;
+    }
+    return len;
+}
